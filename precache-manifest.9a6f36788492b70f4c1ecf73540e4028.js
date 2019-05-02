c6474d884519a7c30357f74f423527909df3f093
hiijs <bernadetta.nycz@icloud.com>
2019-05-02 14:17:28 +0200

    Updates

---
+++ precache-manifest.9a6f36788492b70f4c1ecf73540e4028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac4bc716b1cfe7b6c5b8ce11b3be0bd",
+    "url": "/Hiiiij/routing.git/index.html"
+  },
+  {
+    "revision": "319761b133be7c3bf47f",
+    "url": "/Hiiiij/routing.git/static/css/main.d05ddb39.chunk.css"
+  },
+  {
+    "revision": "970e65e10a0627d2be96",
+    "url": "/Hiiiij/routing.git/static/js/2.1aa2c60b.chunk.js"
+  },
+  {
+    "revision": "319761b133be7c3bf47f",
+    "url": "/Hiiiij/routing.git/static/js/main.57c72bb1.chunk.js"
+  },
+  {
+    "revision": "35f45135fe517cd2f5b1",
+    "url": "/Hiiiij/routing.git/static/js/runtime~main.f0b988d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Hiiiij/routing.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
